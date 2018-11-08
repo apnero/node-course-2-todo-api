061146b89c3719f9ad9fbd93c71c19f1606447cf
Andrew Nero <andrew.nero@gmail.com>
2018-11-07 23:21:14 -0500

    major

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -1,17 +1,17 @@
 // const MongoClient = require('mongodb').MongoClient;
 const {MongoClient, ObjectID} = require('mongodb');
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) =>{
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err) {
-    return console.log('Unable to connect to MongoDB server.');
+    return console.log('Unable to connect to MongoDB server');
   }
   console.log('Connected to MongoDB server');
-  //
+
   // db.collection('Todos').findOneAndUpdate({
-  //   _id: new Object('5be381e82fd3a676e4df37dd')
+  //   _id: new ObjectID('57bc4b15b3b6a3801d8c47a2')
   // }, {
   //   $set: {
-  //     completed: false
+  //     completed: true
   //   }
   // }, {
   //   returnOriginal: false
@@ -20,13 +20,13 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) =>{
   // });
 
   db.collection('Users').findOneAndUpdate({
-    _id: new Object('5be37e556156d8cb20b2b23d')
+    _id: new ObjectID('57abbcf4fd13a094e481cf2c')
   }, {
     $set: {
-      name: 'Thomas'
+      name: 'Andrew'
     },
     $inc: {
-      age: 5
+      age: 1
     }
   }, {
     returnOriginal: false
